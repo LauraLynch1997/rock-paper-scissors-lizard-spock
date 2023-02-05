158dd7611c88fbcbeb9736068eb8eff0973b29f9
LauraLynch1997 <lauralynch1997@gmail.com>
2023-02-05 16:15:10 +0000

    Calculate if difference between inputs is odd or even

---
+++ assets/js/scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("hello");
+    let human = 2;
+    let computer = 4;
+    let difference = Math.abs((human-computer) % 2);
+    console.log(difference); 
+  });
+  
